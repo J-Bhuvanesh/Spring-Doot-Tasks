17b34dd7053ce89031f0641b05624ff9c2cc205f
bhuvanesh <jbhuvaneshkumar1998@gmail.com>
2021-04-29 09:29:24 +0530

    first commit

---
+++ SpringBoot-CRUD-UsingMySql/src/main/java/com/example/crud/SpringBootCrudUsingMySqlApplication.java
@@ -0,0 +1,14 @@
+package com.example.crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCrudUsingMySqlApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCrudUsingMySqlApplication.class, args);
+		System.out.print("running....");
+	}
+
+}
