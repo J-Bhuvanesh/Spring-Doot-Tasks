f836ce8cb2bbbb3bbd84a8ad0e2d0188a7d8d187
bhuvanesh <jbhuvaneshkumar1998@gmail.com>
2021-05-10 23:12:22 +0530

    initial commit

---
+++ JWT-Secutiry/src/main/java/com/JWT/Secutiry/JwtSecutiryApplication.java
@@ -0,0 +1,13 @@
+package com.JWT.Secutiry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtSecutiryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtSecutiryApplication.class, args);
+	}
+
+}
---
+++ JWT-Secutiry/src/main/java/com/JWT/Secutiry/controller/TokenController.java
@@ -0,0 +1,30 @@
+package com.JWT.Secutiry.controller;
+
+
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.JWT.Secutiry.security.JwtGenerator;
+import com.JWT.Secutiry.security.model.JwtUser;
+
+@RestController
+@RequestMapping("/token")
+public class TokenController {
+
+
+    private JwtGenerator jwtGenerator;
+
+    public TokenController(JwtGenerator jwtGenerator) {
+        this.jwtGenerator = jwtGenerator;
+    }
+
+    @PostMapping
+    public String generate(@RequestBody final JwtUser jwtUser) {
+
+        return jwtGenerator.generate(jwtUser);
+
+    }
+}
